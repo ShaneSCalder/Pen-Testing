7549181bcaf8f068232d0423f069439280c96566
Shane Calder <shanescottcalder@gmail.com>
2018-07-17 09:20:57 -0600

    Add files via upload
    changed to make code more stable

--- mac_changer.py
+++ mac_changer.py
@@ -9,6 +9,6 @@ new_mac = input('Enter New MAC i.e. 00:00:00:00:00:00')
 
 print("[+] Changing MAC address for " + interface + 'to ' + new_mac)
 
-subprocess.call('ifconfig' + interface + 'down', shell=True)
-subprocess.call('ifconfig' + interface + 'hw ether' + new_mac, shell=True)
-subprocess.call('ifconfig' + interface + 'wlan0 up', shell=True)
+subprocess.call(['ifconfig', interface, 'down'])
+subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+subprocess.call(['ifconfig', interface, 'up'])
