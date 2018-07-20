265e9e95ed04108756df8d0b06aaf2973cc6cb37
Shane Calder <shanescottcalder@gmail.com>
2018-07-20 16:04:04 -0600

    Add files via upload
    Added:
    scapy layer http
    print http packet(s)

--- Packer_Sniffer.py
+++ Packer_Sniffer.py
@@ -1,11 +1,14 @@
 #1/usr/bin/env python3
 
 import scapy.all as scapy
+from scapy.layers import http
+# use: pip install scapy_http
 
 def sniff(interface):
-    scapy.sniff(iface = interface, store = False, prn = process_sniffed_packet)
+    scapy.sniff(iface = interface, store = False, prn = process_sniffed_packet,)
 
 def process_sniffed_packet(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+        print(packet)
 
 sniff('eth0')
