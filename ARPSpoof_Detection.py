ad9ba93c52816d62ec3f7c6f98747370d851bf1e
Shane Calder <shanescottcalder@gmail.com>
2018-07-29 13:52:08 -0600

    Add files via upload
    Fixed IndexError 
    ToDo 
    add email alert

--- ARPSpoof_Detection.py
+++ ARPSpoof_Detection.py
@@ -15,10 +15,13 @@ def sniff(interface):
 
 def process_sniffed_packet(packet):
     if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2
-    real_mac = get_mac(packet[scapy.ARP].psrc)
-    response_mac = packet[scapy.ARP].hwsrc
+    try:
+        real_mac = get_mac(packet[scapy.ARP].psrc)
+        response_mac = packet[scapy.ARP].hwsrc
 
-    if real_mac != response_mac:
-    print("[+] You are under attack!!")
+        if real_mac != response_mac:
+            print("[+] You are under attack!!")
+    except IndexError:
+        pass
 
 sniff('eth0')
