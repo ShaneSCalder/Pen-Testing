d324b862b4427273a49c5165828e7bb44802f3cb
Shane Calder <shanescottcalder@gmail.com>
2018-07-30 09:48:26 -0600

    Add files via upload
    Added:
    Python link for resource SMTP

--- execute_and_report.py
+++ execute_and_report.py
@@ -5,6 +5,7 @@ import subprocess, smtplib
 
 def send_mail(email, password, message):
     # to use gmail smtp you will need to use 'less secure app' https://support.google.com/a/answer/182076
+    # SMTP python resource https://docs.python.org/2/library/smtplib.html
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(email, passwrod)
